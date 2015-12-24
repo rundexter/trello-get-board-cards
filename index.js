0e3febea8368516fe690367883bf1c44a3d65968
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-24 17:29:15 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+var trello = require('node-trello'),
+    _ = require('lodash'),
+    util = require('./util'),
+    pickInputs = {
+        board_id: 'board_id'
+    },
+    pickOutputs = {
+        '-': {
+            keyName: 'data',
+            fields: {
+                id: 'id',
+                name: 'name',
+                idList: 'idList',
+                url: 'url'
+            }
+        }
+    };
+
 module.exports = {
+    authOptions: function (dexter) {
+        if (!dexter.environment('trello_api_key') || !dexter.environment('trello_token')) {
+            this.fail('A [trello_api_key] or [trello_token] environment variables are required for this module');
+            return false;
+        } else {
+            return {
+                api_key: dexter.environment('trello_api_key'),
+                token: dexter.environment('trello_token')
+            }
+        }
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +35,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authOptions(dexter),
+            getReqFields = "name,idList,url";
+
+        if (!auth) return;
+
+        var t = new trello(auth.api_key, auth.token);
+        var inputs = util.pickStringInputs(step, pickInputs);
+
+        if (_.isEmpty(inputs.board_id)) {
+            return this.fail('A [boardId] variable is required for this module');
+        } else {
+            var boardId = inputs.board_id;
+        }
+
+        t.get("1/boards/" + boardId + "/cards", { fields: getReqFields }  , function(err, data) {
+            if (!err) 
+                this.complete(util.pickResult({data: data}, pickOutputs));
+            else 
+                this.fail(err);
+        }.bind(this));
     }
 };
